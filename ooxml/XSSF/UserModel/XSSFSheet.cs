5574581859aa28a6c27618c487f3dd08fff1687d
Hoshino Izumi <iz-hoshino@toyokeizai.co.jp>
2021-07-01 17:59:12 +0900

    Fixed an issue where non-referencing extended controls causes Library to crash
    (cherry picked from commit 395edb43fece196fa5f1aa2b62befd4f88f59ed2)

--- ooxml/XSSF/UserModel/XSSFSheet.cs
+++ ooxml/XSSF/UserModel/XSSFSheet.cs
@@ -2219,6 +2219,10 @@ namespace NPOI.XSSF.UserModel
 
                 int selectedIndex = 0;
                 var cell = GetLikedCellForControl(fmlaLink);
+                if(cell == null)
+                {
+                    continue;
+                }
                 if (cell.CellType == CellType.Numeric)
                 {
                     selectedIndex = (int)cell.NumericCellValue;
@@ -2255,14 +2259,21 @@ namespace NPOI.XSSF.UserModel
 
                 bool isChecked = false;
                 var cell = GetLikedCellForControl(fmlaLink);
+                if(cell == null)
+                {
+                    continue;
+                }
+
                 if (cell.CellType == CellType.Numeric)
                 {
-                    isChecked = ((int)cell.NumericCellValue) > 0;
+                    isChecked = ((int)cell.NumericCellValue) != 0;
                 }
-                else if (cell.CellType == CellType.String)
+                else if (cell.CellType == CellType.String && !String.IsNullOrEmpty(cell.StringCellValue))
                 {
-                    int.TryParse(cell.StringCellValue, out int v);
-                    isChecked = v > 0;
+                    if(int.TryParse(cell.StringCellValue, out int v))
+                    {
+                        isChecked = v != 0;
+                    }
                 }
                 else if (cell.CellType == CellType.Boolean)
                 {
@@ -2289,6 +2300,10 @@ namespace NPOI.XSSF.UserModel
 
                 int sel = -1;
                 var cell = GetLikedCellForControl(fmlaLink);
+                if(cell == null)
+                {
+                    continue;
+                }
                 if (cell.CellType == CellType.Numeric)
                 {
                     sel = ((int)cell.NumericCellValue);
